7c242bed4e4d7625384ff8c8f104a68d7c69b147
jinyoung <djawls111@gmail.com>
2025-07-23 20:56:27 +0900

    first commit

---
+++ src/main/java/com/tj703/mobit/MobitApplication.java
@@ -0,0 +1,13 @@
+package com.tj703.mobit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tj703/mobit/MobitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj703.mobit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
